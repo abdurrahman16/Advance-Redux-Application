5409c41bb795c7c4f5f49d01fb23109e4fb6ff22
ラハマン <rahman.just16@gmail.com>
2026-01-11 10:33:25 +0900

    reducer action

--- index.js
+++ index.js
@@ -7,7 +7,32 @@ const initialState ={
 
 //reducer
 const rootReducer=(state= initialState,action) => {
+    if (action.type==='INCREMENT'){
+        return {
+            ...state,
+            num: state.num +1
+        }
+    }
+    if (action.type==='ADDNUM'){
+        return {
+            ...state,
+            num: state.num +action.value
+        }
+    }
+        
     return state;
 }
+//store
 const store = redux.createStore(rootReducer);
+console.log(store.getState());
+
+//dispqatching action
+store.dispatch({
+    type:'INCREMENT'
+});
+store.dispatch({
+    type:'ADDNUM',
+    value: 34
+});
+
 console.log(store.getState());
